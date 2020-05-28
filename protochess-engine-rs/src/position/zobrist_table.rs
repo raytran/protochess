22a74d8dad27ac62b4846cb936ad0772d3041915
Raymond Tran <raytran@mit.edu>
2020-05-28 14:12:49 -0700

    Changed zobrist hashing to only use a hashtable with custom pieces. Still a lot of overhead; might change custom pieces to a Vec later

--- protochess-engine-rs/src/position/zobrist_table.rs
+++ protochess-engine-rs/src/position/zobrist_table.rs
@@ -7,8 +7,16 @@ use crate::position::piece::Piece;
 
 //Holds the random numbers used in generating zobrist keys
 pub struct ZobristTable {
+    //zobrist[player_num][piecetype][index] -> zobrist key
+    //king = 0
+    //queen = 1
+    //rook = 2
+    //bishop = 3
+    //knight = 4
+    //pawn = 5
+    zobrist: Vec<Vec<Vec<u64>>>,
     //Map of piecetype -> Vec of length 256, one random # for each index for each playernum
-    zobrist: HashMap<(u8, PieceType), Vec<u64>>,
+    custom_zobrist: HashMap<(u8, PieceType), Vec<u64>>,
     ep_zobrist: Vec<u64>,
     white_to_move: u64,
     w_q_castle: u64,
@@ -25,8 +33,23 @@ impl ZobristTable {
         for _ in 0..=16 {
             ep_zobrist.push(rng.gen::<u64>());
         }
+        let mut zobrist = Vec::with_capacity(4);
+        //TODO 2+ players(?)
+        zobrist.push(Vec::new());
+        zobrist.push(Vec::new());
+        for i in 0..2 {
+            for j in 0..6 {
+                let mut randoms = Vec::with_capacity(256);
+                for _ in 0..=255 {
+                    randoms.push(rng.gen::<u64>());
+                }
+                zobrist[i].push(randoms)
+            }
+        }
+
         let mut table = ZobristTable{
-            zobrist: Default::default(),
+            zobrist,
+            custom_zobrist: Default::default(),
             ep_zobrist,
             white_to_move: rng.gen::<u64>(),
             w_q_castle: rng.gen::<u64>(),
@@ -37,19 +60,6 @@ impl ZobristTable {
         };
         //Initialize the table with the default piece set in a repeatable way
         //Mostly for easier testing
-        table.register_piecetype(0, &PieceType::King);
-        table.register_piecetype(0, &PieceType::Queen);
-        table.register_piecetype(0, &PieceType::Pawn);
-        table.register_piecetype(0, &PieceType::Bishop);
-        table.register_piecetype(0, &PieceType::Knight);
-        table.register_piecetype(0, &PieceType::Rook);
-
-        table.register_piecetype(1, &PieceType::King);
-        table.register_piecetype(1, &PieceType::Queen);
-        table.register_piecetype(1, &PieceType::Pawn);
-        table.register_piecetype(1, &PieceType::Bishop);
-        table.register_piecetype(1, &PieceType::Knight);
-        table.register_piecetype(1, &PieceType::Rook);
         table
     }
 
@@ -69,29 +79,54 @@ impl ZobristTable {
     }
 
     pub fn get_zobrist_sq_from_pt(&mut self, pt: &PieceType, owner: u8, index: u8) -> u64 {
-        if !self.zobrist.contains_key(&(owner, pt.to_owned())) {
-            self.register_piecetype(owner, pt);
+        match pt {
+            PieceType::King => {
+                self.zobrist[owner as usize][0][index as usize]
+            }
+            PieceType::Queen => {
+                self.zobrist[owner as usize][1][index as usize]
+            }
+            PieceType::Rook => {
+                self.zobrist[owner as usize][2][index as usize]
+            }
+            PieceType::Bishop => {
+                self.zobrist[owner as usize][3][index as usize]
+            }
+            PieceType::Knight => {
+                self.zobrist[owner as usize][4][index as usize]
+            }
+            PieceType::Pawn => {
+                self.zobrist[owner as usize][5][index as usize]
+            }
+            PieceType::Custom(c) => {
+                if !self.custom_zobrist.contains_key(&(owner, pt.to_owned())) {
+                    self.register_piecetype(owner, pt);
+                }
+                self.custom_zobrist.get(&(owner, pt.to_owned())).unwrap()[index as usize]
+            }
         }
-        self.zobrist.get(&(owner, pt.to_owned())).unwrap()[index as usize]
     }
 
     pub fn get_zobrist_sq(&mut self, piece:&Piece, index:u8) -> u64 {
-        if !self.zobrist.contains_key(&(piece.player_num, (&piece.piece_type).to_owned())) {
-            self.register_piecetype(piece.player_num, &piece.piece_type);
-        }
-        self.zobrist.get(&(piece.player_num, (&piece.piece_type).to_owned())).unwrap()[index as usize]
+        self.get_zobrist_sq_from_pt(&piece.piece_type, piece.player_num, index)
     }
 
     pub fn get_ep_zobrist_file(&mut self, rank: u8) -> u64 {
         self.ep_zobrist[rank as usize]
     }
 
+    //Registers a custom piece type
     fn register_piecetype(&mut self, player_num:u8, pt:&PieceType){
+        let randoms = self.make_randoms();
+        self.custom_zobrist.insert((player_num, pt.to_owned()), randoms);
+    }
+
+    fn make_randoms(&mut self) -> Vec<u64> {
         let mut randoms = Vec::with_capacity(256);
         for i in 0..=255 {
             randoms.push(self.rng.gen::<u64>());
         }
-        self.zobrist.insert((player_num, pt.to_owned()), randoms);
+        randoms
     }
 }
 
