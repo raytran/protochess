431ae35d5247c9260391c1e2ca96418c9cf8512c
raymond <raytran@mit.edu>
2020-06-28 23:42:44 -0700

    Changed to absolute URL for image

--- protochess-front/src/components/Chess/Piece.svelte
+++ protochess-front/src/components/Chess/Piece.svelte
@@ -8,7 +8,7 @@
         if (piece.piece_type.length === 1){
             return piece.owner === 0 ? "/images/chess_pieces/white/" + piece.piece_type + ".svg" : "/images/chess_pieces/black/" + piece.piece_type + ".svg";
         }else{
-            return 'images/chess_pieces/' +piece.piece_type + '.svg';
+            return '/images/chess_pieces/' +piece.piece_type + '.svg';
         }
     })()
 
