9d3674ab8a0b20d523e37e1938e2188d0c88ea56
raymond <raytran@mit.edu>
2020-06-30 21:59:11 -0700

    Added basic heartbeat to websocket to stop cloudflare from closing connections.

--- protochess-server-rs/src/client.rs
+++ protochess-server-rs/src/client.rs
@@ -5,13 +5,16 @@ use crate::client_message::ClientResponse;
 pub struct Client {
     pub(crate) name: String,
     pub(crate) id: Uuid,
-    pub(crate) sender: mpsc::UnboundedSender<ClientResponse>
+    pub(crate) sender: mpsc::UnboundedSender<Result<warp::ws::Message, warp::Error>>
 }
 
 impl Client {
     pub fn try_send(&self, cr: ClientResponse){
-        if let Err(_) = self.sender.send(cr){
-            eprintln!("client send error????");
+        if let Ok(text) = serde_json::to_string(&cr){
+            let msg = Ok(warp::ws::Message::text(text));
+            if let Err(_) = self.sender.send(msg){
+                eprintln!("client send error????");
+            }
         }
     }
 }
--- protochess-server-rs/src/websockets.rs
+++ protochess-server-rs/src/websockets.rs
@@ -2,23 +2,20 @@ use tokio::sync::{mpsc};
 use futures::{FutureExt, StreamExt};
 use warp::ws::WebSocket;
 use std::sync::{ Arc };
+use std::sync::atomic::{AtomicU16, Ordering};
 use crate::{Rooms};
 use crate::client_message::{ClientResponse, ClientRequest};
 use crate::client::Client;
 use uuid::Uuid;
 use crate::room_message::RoomMessage;
+use tokio::sync::oneshot::error::TryRecvError;
 
 pub async fn user_connected(ws: WebSocket, rooms: Rooms){
     //Bind mpsc channel to websocket
     let (web_tx, web_rx) = mpsc::unbounded_channel();
     let (user_ws_tx, mut user_ws_rx) = ws.split();
     tokio::task::spawn(
-        web_rx
-            .map(|x: ClientResponse| {
-                serde_json::to_string(&x).unwrap()
-            })
-            .map(|x| Ok(warp::ws::Message::text(x)))
-            .forward(user_ws_tx)
+        web_rx.forward(user_ws_tx)
             .map(|result| {
                 if let Err(e) = result {
                     eprintln!("websocket send error: {}", e);
@@ -26,23 +23,50 @@ pub async fn user_connected(ws: WebSocket, rooms: Rooms){
             })
     );
 
+    //Heartbeat
+    let ping_counter = Arc::new(AtomicU16::new(0));
+    let pong_counter = Arc::new(AtomicU16::new(0));
+
+    let tx = web_tx.clone();
+    let ping_cnt = ping_counter.clone();
+    let pong_cnt = pong_counter.clone();
+    //hb_tx goes out of scope, ending the task
+    let (hb_tx, mut hb_rx) = tokio::sync::oneshot::channel::<()>();
+    tokio::spawn(async move {
+        let mut heartbeat_interval = tokio::time::interval(tokio::time::Duration::from_secs(5));
+        loop {
+            heartbeat_interval.tick().await;
+            tx.send(Ok(warp::ws::Message::ping(Vec::new())));
+            ping_cnt.fetch_add(1, Ordering::Relaxed);
+            let pings = ping_cnt.load(Ordering::Relaxed);
+            let pongs = pong_cnt.load(Ordering::Relaxed);
+            if pings > pongs + 1 + 5 {
+                //this user is probably not alive
+                println!("One sided disconnect");
+                //TODO make ws actually disconnect...
+                break;
+            }
+            match hb_rx.try_recv() {
+                // The channel will never receive a value.
+                Ok(_) | Err(TryRecvError::Closed) => {
+                    break;
+                }
+                Err(TryRecvError::Empty) => {}
+            }
+        }
+    });
+
     //This Client properties
     let my_id = Uuid::new_v4();
     let mut my_room:Option<(String, mpsc::UnboundedSender<RoomMessage>)> = None;
-    let mut generator = adjective_adjective_animal::Generator::default();
-    let name = {
-        if let Some(name) = generator.next() {
-            name
-        }else{
-            "Anon".to_string()
-        }
-    };
+    let name = generate_name();
     //Insert client to global map of clients
     let my_client = Arc::new(Client{
         name,
         id: my_id.clone(),
         sender: web_tx.clone()
     });
+
     {
         let mut rms = rooms.write().await;
         rms.register_broadcast_rooms(my_id, my_client.clone()).await;
@@ -58,7 +82,11 @@ pub async fn user_connected(ws: WebSocket, rooms: Rooms){
             }
         };
 
-        if let Ok(str) = msg.to_str(){
+        if msg.is_pong() {
+            pong_counter.fetch_add(1, Ordering::Relaxed);
+        }
+
+        if let Ok(str) = msg.to_str() {
             let res:serde_json::Result<ClientRequest> = serde_json::from_str(str);
             if let Ok(cm) = res {
                 match cm {
@@ -156,3 +184,11 @@ pub async fn user_connected(ws: WebSocket, rooms: Rooms){
     rms.unregister_broadcast_rooms(&my_id).await;
 }
 
+fn generate_name() -> String {
+    let mut generator = adjective_adjective_animal::Generator::default();
+    if let Some(name) = generator.next() {
+        name
+    }else{
+        "Anon".to_string()
+    }
+}
