2cd0dc04f5052852817b15ab668835acf2921f1f
raymond <raytran@mit.edu>
2020-07-18 03:10:48 -0700

    Removed $ from variable names

--- protochess-front/src/pages/_layout.svelte
+++ protochess-front/src/pages/_layout.svelte
@@ -9,27 +9,26 @@
             ]
     import { onMount,afterUpdate } from 'svelte'
     onMount(() => {
-        const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+        const navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
         // Check if there are any navbar burgers
-        if ($navbarBurgers.length > 0) {
+        if (navbarBurgers.length > 0) {
             // Add a click event on each of them
-            $navbarBurgers.forEach( el => {
+            navbarBurgers.forEach( el => {
                 el.addEventListener('click', () => {
                     // Get the target from the "data-target" attribute
-                    const target = el.dataset.target;
-                    const $target = document.getElementById(target);
+                    const target = document.getElementById(el.dataset.target);
                     // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
                     el.classList.toggle('is-active');
-                    $target.classList.toggle('is-active');
+                    target.classList.toggle('is-active');
                 });
             });
         }
     });
     // in case you want to close it after user selects a different menu option
     afterUpdate(() => {
-        const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger.is-active,.navbar-menu.is-active'), 0);
-        if ($navbarBurgers.length > 0) {
-            $navbarBurgers.forEach( el => {
+        const navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger.is-active,.navbar-menu.is-active'), 0);
+        if (navbarBurgers.length > 0) {
+            navbarBurgers.forEach( el => {
                 el.classList.toggle('is-active');
             });
         }
